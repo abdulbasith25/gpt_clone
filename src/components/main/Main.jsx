a1d1a7ef650385dedf727321c6d9e6d2adbe9769
abdul basith <abdulbasith2590@gmail.com>
2025-09-30 09:55:20 +0530

    new

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -21,7 +21,7 @@ const Main = () => {
 	return (
 		<div className="main">
 			<div className="nav">
-				<p>Gemini</p>
+				<p>Gemini By Basith</p>
 				<img src={assets.user} alt="User" />
 			</div>
 			<div className="main-container">
